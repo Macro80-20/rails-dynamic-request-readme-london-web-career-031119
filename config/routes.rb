def79546bd72f9621f414389040f6e21716492d1
Fernando <fernando_lopezc@live.com>
2019-04-08 15:45:53 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # our routying system now has a route that can receieve a parameter and the parameter will be passed to the controllers show action .
+  # get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
 end
